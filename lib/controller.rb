55868ce6eefa53ca99e5bdd36b08272aab32713d
Sylvain.D <sylvain.deligny@yahoo.fr>
2023-10-23 09:38:32 +0200

    style. again and comments

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,30 +1,43 @@
+# On commence par importer le fichier Gossip.rb, qui contient la classe Gossip que nous avons définie précédemment.
 require 'gossip'
 
+# Ensuite, on définit un contrôleur (controller) nommé ApplicationController en utilisant la bibliothèque Sinatra.
 class ApplicationController < Sinatra::Base
+    # Cette route correspond à la page d'accueil, qui affiche tous les potins.
     get '/' do
+        # On utilise la méthode Gossip.all pour récupérer tous les potins et on les stocke dans une variable gossips.
         erb :index, locals: { gossips: Gossip.all }
     end
     
+    # Cette route correspond à la page de création d'un nouveau potin.
     get '/gossips/new' do 
         erb :new_gossip
     end
 
+    # Cette route est une route dynamique qui permet d'afficher un potin spécifique en fonction de son ID.
     get '/gossips/:id' do
+      # On récupère l'ID du potin depuis les paramètres de l'URL et on le convertit en entier.
       id = params[:id].to_i
+      # On utilise la méthode Gossip.all pour récupérer tous les potins, puis on sélectionne celui qui correspond à l'ID.
       @potin = Gossip.all[id]
       erb :show
     end
+
+    # Cette route gère la soumission du formulaire de création d'un potin.
     post '/gossips/new' do
       author = params[:gossip_author]
       content = params[:gossip_content]
     
+      # On vérifie si l'auteur ou le contenu est vide. Si l'un d'entre eux est vide, on redirige vers le formulaire de création.
       if author.empty? || content.empty?
-        redirect '/gossips/new'  #vers le formulaire de création
+        redirect '/gossips/new'  # Redirection vers le formulaire de création
       else
+        # Si les champs ne sont pas vides, on crée un nouvel objet Gossip avec les données de l'auteur et du contenu.
         gossip = Gossip.new(author, content)
+        # On sauvegarde le potin dans le fichier CSV.
         gossip.save
+        # Enfin, on redirige l'utilisateur vers la page d'accueil.
         redirect '/'
       end
     end
-
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,25 +1,41 @@
+# On commence par importer la bibliothèque CSV, qui nous permet de travailler avec des fichiers CSV.
 require 'csv'
 
+# Ensuite, on définit une classe appelée Gossip, qui sera utilisée pour représenter nos potins.
 class Gossip
-    attr_accessor :author, :content
-  
-    def initialize(author, content)
-      @content = content
-      @author = author
+  # Les deux lignes suivantes créent deux attributs (variables) pour notre classe : author (auteur) et content (contenu).
+  attr_accessor :author, :content
+
+  # Le constructeur initialize est une méthode spéciale qui est appelée lorsqu'on crée une nouvelle instance de la classe.
+  def initialize(author, content)
+    # On attribue les valeurs passées en argument aux attributs de l'instance (l'objet).
+    @content = content
+    @author = author
+  end
+
+  # La méthode save permet de sauvegarder un potin dans un fichier CSV.
+  def save
+    # On ouvre le fichier "gossip.csv" en mode d'ajout ("a").
+    # Cela signifie qu'on ajoutera de nouvelles lignes au fichier sans effacer les précédentes.
+    CSV.open("./db/gossip.csv", "a") do |csv|
+      # On ajoute les valeurs de l'auteur et du contenu du potin en tant que nouvelle ligne dans le fichier CSV.
+      csv << [@author, @content]
+    end
+  end
+
+  # La méthode self.all permet de récupérer tous les potins existants depuis le fichier CSV.
+  def self.all
+    # On crée un tableau vide pour stocker tous les potins.
+    all_gossips = []
+
+    # On lit le fichier CSV ligne par ligne.
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      # Pour chaque ligne du CSV, on crée un nouvel objet Gossip en utilisant les valeurs de l'auteur et du contenu de la ligne.
+      # Ensuite, on ajoute cet objet au tableau all_gossips.
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
     end
 
-    def save
-        CSV.open("./db/gossip.csv", "a") do |csv|
-          csv << [@author, @content]
-        end
-    end
-
-    def self.all
-        all_gossips = []
-        CSV.read("./db/gossip.csv").each do |csv_line|
-          all_gossips << Gossip.new(csv_line[0], csv_line[1])
-        end
-        return all_gossips
-    end
-end    
-  
+    # Enfin, on retourne le tableau complet des potins.
+    return all_gossips
+  end
+end
